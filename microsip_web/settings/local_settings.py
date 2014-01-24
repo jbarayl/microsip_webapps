93733eced12caf05b3f0b8314a91a8cb4af76fc2
Jesús Herrera <jesusmaherrera@gmail.com>
2014-01-24 10:01:58 -0700

    se modifico settings

--- microsip_web/settings/local_settings.py
+++ microsip_web/settings/local_settings.py
@@ -2,13 +2,15 @@ MICROSIP_MODULES = (
     'microsip_web.apps.main',
     'microsip_web.apps.config',
     # 'microsip_web.apps.main.filtros',
+    # 'microsip_web.apps.main.servicios',
     # 'microsip_web.apps.inventarios',
-    # 'microsip_web.apps.inventarios.almacen_clon',
     # 'microsip_web.apps.ventas',
     # 'microsip_web.apps.cuentas_por_pagar',
     # 'microsip_web.apps.cuentas_por_cobrar',
-    # 'microsip_web.apps.contabilidad',
+    #'microsip_web.apps.contabilidad',
+    
     # 'microsip_web.apps.punto_de_venta',
-    #'microsip_web.apps.punto_de_venta.puntos',
+    # 'microsip_web.apps.punto_de_venta.puntos',
     # 'microsip_web.apps.punto_de_venta.generar_polizas',
+    # 'microsip_web.apps.compras',
 )
